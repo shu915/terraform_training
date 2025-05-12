9c659579c877a19e94edc8049c2b1e3af78132bb
shu915 <chukichi915@gmail.com>
2025-05-12 12:12:05 +0900

    オートスケーリングを実装

--- appserver.tf
+++ appserver.tf
@@ -39,19 +39,81 @@ resource "aws_ssm_parameter" "password" {
   value = "aws_db_instance.mysql_standalone.password"
 }
 
-resource "aws_instance" "app_server" {
-  ami                         = data.aws_ami.app.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public_subnet-1a.id
-  vpc_security_group_ids      = [aws_security_group.app_sg.id, aws_security_group.opnmg_sg.id]
-  key_name                    = aws_key_pair.keypair.key_name
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.app_ec2_profile.name
+# resource "aws_instance" "app_server" {
+#   ami                         = data.aws_ami.app.id
+#   instance_type               = "t3.micro"
+#   subnet_id                   = aws_subnet.public_subnet-1a.id
+#   vpc_security_group_ids      = [aws_security_group.app_sg.id, aws_security_group.opnmg_sg.id]
+#   key_name                    = aws_key_pair.keypair.key_name
+#   associate_public_ip_address = true
+#   iam_instance_profile        = aws_iam_instance_profile.app_ec2_profile.name
 
-  tags = {
-    Name        = "${var.project}-${var.environment}-app-server"
-    Project     = var.project
-    Environment = var.environment
-    Type        = "app"
+#   tags = {
+#     Name        = "${var.project}-${var.environment}-app-server"
+#     Project     = var.project
+#     Environment = var.environment
+#     Type        = "app"
+#   }
+# }
+
+resource "aws_launch_template" "app_lt" {
+  update_default_version = true
+
+  name = "${var.project}-${var.environment}-app-lt"
+
+  image_id = data.aws_ami.app.id
+
+  key_name = aws_key_pair.keypair.key_name
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name        = "${var.project}-${var.environment}-app-ec2"
+      Project     = var.project
+      Environment = var.environment
+      Type        = "app"
+    }
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.app_sg.id, aws_security_group.opnmg_sg.id]
+    delete_on_termination       = true
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.app_ec2_profile.name
+  }
+
+  user_data = filebase64("${path.module}/src/initialize.sh")
+}
+
+resource "aws_autoscaling_group" "app_asg" {
+  name = "${var.project}-${var.environment}-app-asg"
+
+  max_size         = 1
+  min_size         = 1
+  desired_capacity = 1
+
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+
+  vpc_zone_identifier = [aws_subnet.public_subnet-1a.id, aws_subnet.public_subnet-1c.id]
+
+  target_group_arns = [aws_lb_target_group.alb_target_group.arn]
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.app_lt.id
+        version            = "$Latest"
+      }
+      override {
+        instance_type = "t3.micro"
+      }
+    }
   }
 }
+
+
+
--- data.tf
+++ data.tf
@@ -6,26 +6,31 @@ data "aws_ami" "app" {
   most_recent = true
   owners      = ["self", "amazon"]
 
-  filter {
-    name = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20250414.0-x86_64-gp2"
-    ]
-  }
 
   filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    name   = "name"
+    values = ["tastylog-app-ami"]
   }
+  # filter {
+  #   name = "name"
+  #   values = ["amzn2-ami-kernel-5.10-hvm-2.0.20250414.0-x86_64-gp2"
+  #   ]
+  # }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  # filter {
+  #   name   = "root-device-type"
+  #   values = ["ebs"]
+  # }
 
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
+  # filter {
+  #   name   = "virtualization-type"
+  #   values = ["hvm"]
+  # }
+
+  # filter {
+  #   name   = "architecture"
+  #   values = ["x86_64"]
+  # }
 
 
 
